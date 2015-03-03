9cdcdc36d1ee2003e8bdfbd00e253c13891c6ce1
Chris Foster <cdbfoster@gmail.com>
2015-03-03 13:28:45 -0500

    Implemented the Activity class.

--- Activity.cpp
+++ Activity.cpp
@@ -16,3 +16,143 @@
 *
 * Copyright 2015 Chris Foster
 */
+
+#include "Activity.hpp"
+#include "Schedule.hpp"
+
+using namespace Schedule;
+
+Activity::Activity() :
+	Name("Activity"),
+	DesiredLength(Duration(1, 0, 0)),
+	Beginning(NULL)
+{
+	this->ActivityStartMode = StartMode::FREE;
+	this->ActivityLengthMode = LengthMode::FREE;
+
+	this->Owner = NULL;
+}
+
+
+Activity::Activity(Activity const &Other)
+{
+	*this = Other;
+
+	if (Other.Beginning != NULL)
+		this->Beginning = new Offset(*Other.Beginning);
+
+	this->Owner = NULL;
+}
+
+
+Activity::~Activity()
+{
+	if (this->Beginning != NULL)
+		delete this->Beginning;
+}
+
+
+std::string	Activity::GetName() const					{ return this->Name; }
+void		Activity::SetName(std::string const &Name)	{ this->Name = Name; }
+
+
+Activity::StartMode Activity::GetStartMode() const { return this->ActivityStartMode; }
+
+
+void Activity::SetStartMode(StartMode Mode)
+{
+	this->ActivityStartMode = Mode;
+	this->UpdateSchedule();
+}
+
+
+Activity::LengthMode Activity::GetLengthMode() const { return this->ActivityLengthMode; }
+
+
+void Activity::SetLengthMode(LengthMode Mode)
+{
+	this->ActivityLengthMode = Mode;
+	this->UpdateSchedule();
+}
+
+
+Duration	Activity::GetDesiredLength() const		{ return this->DesiredLength; }
+Offset		Activity::GetDesiredStartTime() const	{ return this->DesiredStartTime; }
+Offset		Activity::GetDesiredEndTime() const		{ return this->DesiredEndTime; }
+
+
+void Activity::SetDesiredLength(Duration const &Length)
+{
+	this->DesiredLength = Length;
+	this->UpdateSchedule();
+}
+
+
+void Activity::SetDesiredStartTime(Offset const &StartTime)
+{
+	this->DesiredStartTime = StartTime;
+	this->UpdateSchedule();
+}
+
+
+void Activity::SetDesiredEndTime(Offset const &EndTime)
+{
+	this->DesiredEndTime = EndTime;
+	this->UpdateSchedule();
+}
+
+
+Duration	Activity::GetActualLength() const		{ return this->ActualLength; }
+Offset		Activity::GetActualEndTime() const		{ return this->ActualEndTime; }
+
+
+Offset Activity::GetActualStartTime() const
+{
+	if (this->Beginning != NULL)
+		return *this->Beginning;
+	else
+		return this->ActualStartTime;
+}
+
+
+Duration	Activity::GetOptimalLength() const		{ return this->OptimalLength; }
+Offset		Activity::GetOptimalStartTime() const	{ return this->OptimalStartTime; }
+Offset		Activity::GetOptimalEndTime() const		{ return this->OptimalEndTime; }
+
+
+void Activity::SetActualLength(Duration const &Length)		{ this->ActualLength = Length; }
+void Activity::SetActualStartTime(Offset const &StartTime)	{ this->ActualStartTime = StartTime; }
+void Activity::SetActualEndTime(Offset const &EndTime)		{ this->ActualEndTime = EndTime; }
+
+
+void Activity::SetOptimalLength(Duration const &Length)		{ this->OptimalLength = Length; }
+void Activity::SetOptimalStartTime(Offset const &StartTime)	{ this->OptimalStartTime = StartTime; }
+void Activity::SetOptimalEndTime(Offset const &EndTime)		{ this->OptimalEndTime = EndTime; }
+
+
+Offset const *Activity::GetBeginning() const { return this->Beginning; }
+
+
+void Activity::SetBeginning(Offset const &Beginning)
+{
+	if (this->Beginning != NULL)
+		*this->Beginning = Beginning;
+	else
+		this->Beginning = new Offset(Beginning);
+}
+
+
+void Activity::ClearBeginning()
+{
+	if (this->Beginning != NULL)
+		delete this->Beginning;
+
+	this->Beginning = NULL;
+}
+
+
+void Activity::UpdateSchedule()
+{
+	if (this->Owner != NULL)
+		this->Owner->Update();
+}
