45f1551684a794c075e4f8b8c8d27d355de65dc2
Chris Foster <cdbfoster@gmail.com>
2015-03-03 13:04:55 -0500

    Implemented the Offset class.

--- Offset.cpp
+++ Offset.cpp
@@ -16,3 +16,207 @@
 *
 * Copyright 2015 Chris Foster
 */
+
+#include "Offset.hpp"
+
+using namespace Schedule;
+
+Offset::Offset()
+{
+	this->Hours = 0;
+	this->Minutes = 0;
+	this->Seconds = 0;
+
+	this->Sign = 1;
+}
+
+
+Offset::Offset(long Hours, long Minutes, long Seconds, bool Negative)
+{
+	this->Sign = 1;
+	this->Initialize(Hours, Minutes, Seconds, Negative);
+}
+
+
+void Offset::Initialize(long Hours, long Minutes, long Seconds, bool Negative)
+{
+	// View everything in a "positive" context
+	if (Negative)
+	{
+		Seconds *= -1;
+		Minutes *= -1;
+		Hours *= -1;
+	}
+
+	// Add any overflows to the next denomination
+	Minutes += Seconds / 60;
+	Hours += Minutes / 60;
+
+	// Wrap to the maximum value in each denomination
+	Seconds %= 60;
+	Minutes %= 60;
+
+	// Carry from the next denomination to fill any negatives
+	if (Seconds < 0) { Minutes -= 1; Seconds += 60; }
+	if (Minutes < 0) { Hours -= 1; Minutes += 60; }
+	if (Hours < 0)
+	{
+		this->Sign = -1;
+		this->Initialize(Hours, Minutes, Seconds, true);
+		return;
+	}
+
+	// Now all values are positive and Sign contains the appropriate multiplier
+	this->Hours = Hours;
+	this->Minutes = (short)Minutes;
+	this->Seconds = (short)Seconds;
+}
+
+
+long Offset::GetHours() const			{ return Sign * Hours; }
+long Offset::GetMinutes() const		{ return Sign * Minutes; }
+long Offset::GetSeconds() const		{ return Sign * Seconds; }
+
+
+long Offset::GetTotalMinutes() const		{ return (60 * this->GetHours()) + this->GetMinutes(); }
+long Offset::GetTotalSeconds() const		{ return (60 * this->GetTotalMinutes()) + this->GetSeconds(); }
+
+
+Duration Offset::GetHourRemainder() const	{ return Duration(0, Minutes, Seconds, (Sign > 0 ? false : true)); }
+Duration Offset::GetMinuteRemainder() const	{ return Duration(0, 0, Seconds, (Sign > 0 ? false : true)); }
+
+
+Offset Offset::operator+(Offset const &b) const
+{
+	return Offset(this->GetHours() + b.GetHours(),
+				  this->GetMinutes() + b.GetMinutes(),
+				  this->GetSeconds() + b.GetSeconds());
+}
+
+
+Offset &Offset::operator+=(Offset const &b)
+{
+	*this = *this + b;
+	return *this;
+}
+
+
+Offset Offset::operator-(Offset const &b) const
+{
+	return Offset(this->GetHours() - b.GetHours(),
+				  this->GetMinutes() - b.GetMinutes(),
+				  this->GetSeconds() - b.GetSeconds());
+}
+
+
+Offset &Offset::operator-=(Offset const &b)
+{
+	*this = *this - b;
+	return *this;
+}
+
+
+Offset Offset::operator*(float b) const
+{
+	float const Hours			= b * this->GetHours();
+	float const Minutes			= b * this->GetMinutes() + 60 * (Hours - (long)Hours);
+	float const Seconds			= b * this->GetSeconds() + 60 * (Minutes - (long)Minutes);
+
+	return Offset(Hours, Minutes, Seconds);
+}
+
+
+Offset &Offset::operator*=(float b)
+{
+	*this = *this * b;
+	return *this;
+}
+
+
+Offset Offset::operator/(float b) const
+{
+	return *this * (1.0 / b);
+}
+
+
+Offset &Offset::operator/=(float b)
+{
+	*this = *this / b;
+	return *this;
+}
+
+
+bool Offset::operator<(Offset const &b) const
+{
+	if (this->GetHours() == b.GetHours())
+	{
+		if (this->GetMinutes() == b.GetMinutes())
+		{
+			return this->GetSeconds() < b.GetSeconds();
+		}
+		else
+			return this->GetMinutes() < b.GetMinutes();
+	}
+	else
+		return this->GetHours() < b.GetHours();
+}
+
+
+bool Offset::operator>(Offset const &b) const
+{
+	if (this->GetHours() == b.GetHours())
+	{
+		if (this->GetMinutes() == b.GetMinutes())
+		{
+			return this->GetSeconds() > b.GetSeconds();
+		}
+		else
+			return this->GetMinutes() > b.GetMinutes();
+	}
+	else
+		return this->GetHours() > b.GetHours();
+}
+
+
+bool Offset::operator<=(Offset const &b) const
+{
+	return !(*this > b);
+}
+
+
+bool Offset::operator>=(Offset const &b) const
+{
+	return !(*this < b);
+}
+
+
+bool Offset::operator==(Offset const &b) const
+{
+	if (this->GetHours() != b.GetHours())
+		return false;
+	if (this->GetMinutes() != b.GetMinutes())
+		return false;
+	return this->GetSeconds() == b.GetSeconds();
+}
+
+
+bool Offset::operator!=(Offset const &b) const
+{
+	return !(*this == b);
+}
+
+
+bool Offset::IsNegative() const
+{
+	return (Sign < 0 ? true : false);
+}
+
+
+bool Offset::IsZero() const
+{
+	if (this->GetHours() != 0)
+		return false;
+	if (this->GetMinutes() != 0)
+		return false;
+	return this->GetSeconds() == 0;
+}
--- Offset.hpp
+++ Offset.hpp
@@ -16,3 +16,79 @@
 *
 * Copyright 2015 Chris Foster
 */
+
+#include <iostream>
+
+namespace Schedule
+{
+	class Offset;
+	typedef Offset Duration;
+
+	class Offset
+	{
+	public:
+		Offset();
+		Offset(long Hours, long Minutes, long Seconds, bool Negative = false);
+		~Offset() { }
+
+		// Returns normalized values of each denomination
+		long GetHours() const;
+		long GetMinutes() const;
+		long GetSeconds() const;
+
+		// Returns the entire Offset represented as the requested denomination.  Smaller denominations are truncated.
+		long GetTotalMinutes() const;
+		long GetTotalSeconds() const;
+
+		Duration GetHourRemainder() const;
+		Duration GetMinuteRemainder() const;
+
+		Offset	operator+(Offset const &b) const;
+		Offset &operator+=(Offset const &b);
+
+		Offset	operator-(Offset const &b) const;
+		Offset &operator-=(Offset const &b);
+
+		Offset	operator*(float b) const;
+		Offset &operator*=(float b);
+
+		Offset	operator/(float b) const;
+		Offset &operator/=(float b);
+
+		bool operator<(Offset const &b) const;
+		bool operator>(Offset const &b) const;
+		bool operator<=(Offset const &b) const;
+		bool operator>=(Offset const &b) const;
+		bool operator==(Offset const &b) const;
+		bool operator!=(Offset const &b) const;
+
+		bool IsNegative() const;
+		bool IsZero() const;
+
+	private:
+		long	Hours;
+		short	Minutes;
+		short	Seconds;
+
+		short	Sign;
+
+		void Initialize(long Hours, long Minutes, long Seconds, bool Negative);
+	};
+
+
+	inline std::ostream &operator<<(std::ostream &a, Offset const &b)
+	{
+		short const Sign = (b.IsNegative() ? -1 : 1);
+		long const Hours = std::abs(b.GetHours());
+		long const Minutes = std::abs(b.GetMinutes());
+		long const Seconds = std::abs(b.GetSeconds());
+
+		a << (Sign == -1 ? "-" : "");
+
+		a << (Hours < 10 ? "0" : "") << Hours << ":" <<
+			 (Minutes < 10 ? "0" : "") << Minutes << ":" <<
+			 (Seconds < 10 ? "0" : "") << Seconds;
+
+		return a;
+	}
+}
